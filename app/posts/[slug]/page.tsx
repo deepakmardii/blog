2e893237dfbaf09cff7b8746cfdc8ff4522e4709
Deepak Mardi <deepakmardi22@gmail.com>
2024-02-18 13:38:28 +0530

    added gray matter to parse metadata

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,20 +1,22 @@
 import fs from "fs";
+import matter from "gray-matter";
 import Markdown from "markdown-to-jsx";
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
   const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync(file, "utf-8");
-  return content;
+  const content = fs.readFileSync(file, "utf8");
+  const matterResult = matter(content);
+  return matterResult;
 };
 
 const page = (props: any) => {
   const slug = props.params.slug;
-  const pageContent = getPostContent(slug);
+  const post = getPostContent(slug);
   return (
     <div>
-      This is page content: {slug}
-      <Markdown>{pageContent}</Markdown>
+      <h1>{post.data.title}</h1>
+      <Markdown>{post.content}</Markdown>
     </div>
   );
 };
