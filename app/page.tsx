d6436746309cd84d9bdb451052ddcedf088a305c
Deepak Mardi <deepakmardi22@gmail.com>
2024-02-18 18:02:27 +0530

    sorting of posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
 import getPostMetadata from "@/common/getPostMetadata";
 import PostPreview from "./component/PostPreview";
 
 const page = () => {
   const postMetadata = getPostMetadata();
-  const postPreviews = postMetadata.map((post) => (
-    <PostPreview key={post.slug} {...post} />
-  ));
+  const postPreviews = postMetadata
+    .map((post) => <PostPreview key={post.slug} {...post} />)
+    .reverse(); // Reverse the order of postPreviews
   return <div className=" grid md:grid-cols-2 gap-5 pb-20">{postPreviews}</div>;
 };
 
